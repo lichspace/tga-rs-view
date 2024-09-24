7eddc3a1475a8bca9c597624ba4c2a86da907285
tangentguo-win <tangentguo@tencent.com>
2024-09-24 19:55:18 +0800

    feat: Array3

--- examples/example.rs
+++ examples/example.rs
@@ -1,14 +1,27 @@
 use std::time::Instant;
 
-use image::Rgba;
+use image::{ImageBuffer, Rgba};
+use ndarray::Array3;
 use tgaviewer;
 
 fn main() {
     let image = image::open("dist/D0009.png").unwrap().to_rgba8();
+    let arr = image_to_ndarray(image.clone());
     let start = Instant::now();
     let x = 3062;
     let y = 1102;
     tgaviewer::cv::flood_fill(&image, x, y, Rgba([255, 0, 0, 255]));
     let duration = start.elapsed();
     println!("Execution time: {:?}", duration);
+}
+
+fn image_to_ndarray(img: ImageBuffer<Rgba<u8>, Vec<u8>>) -> Array3<u8> {
+    let (width, height) = img.dimensions();
+
+    // Create an ndarray from the raw pixel data
+    let res = Array3::from_shape_vec((height as usize, width as usize, 4), img.into_raw()).unwrap();
+
+    println!("{:?}", res[[300, 300, 3]]);
+    
+    return res;
 }
